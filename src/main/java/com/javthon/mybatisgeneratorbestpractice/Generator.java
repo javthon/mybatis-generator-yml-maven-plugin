516b2e21594186bc45e8b763aadb38ce136d8912
Javthon <15531972522qq.com>
2020-09-09 13:15:04 +0800

    update

--- src/main/java/com/javthon/mybatisgeneratorbestpractice/Generator.java
+++ src/main/java/com/javthon/mybatisgeneratorbestpractice/Generator.java
@@ -4,7 +4,6 @@ import com.javthon.mybatisgeneratorbestpractice.utils.ConfigurationParser;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
-import org.mybatis.generator.config.Context;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.InputStream;
@@ -25,11 +24,6 @@ public class Generator {
         log.info("Config file generated");
         log.info("Parsing config file, please wait");
         Configuration config = cp.parseConfiguration(xml);
-        Context context = config.getContexts().get(0);
-        // set package name
-        context.getJavaModelGeneratorConfiguration(). setTargetProject(context.getJavaModelGeneratorConfiguration().getTargetProject());
-        context.getSqlMapGeneratorConfiguration().setTargetProject(context.getSqlMapGeneratorConfiguration().getTargetProject());
-        context.getJavaClientGeneratorConfiguration().setTargetProject(context.getJavaClientGeneratorConfiguration().getTargetProject());
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
